b1109b36a94811c5068451ea35738311bb5f5323
JUSIN-20210122V\Administrator <wades9951@gmail.com>
2021-04-05 11:47:44 +0900

    ..

---
+++ Client/Header/TestObject.h
@@ -0,0 +1,31 @@
+#ifndef __TEST_OBJECT_H__
+#define __TEST_OBJECT_H__
+#include "GameObject.h"
+#include "RenderInterface.h"
+
+class TestObject : public ENGINE::GameObject ,
+				   public ENGINE::RenderInterface				
+{
+private:
+	std::shared_ptr<ENGINE::StaticMesh> _StaticMesh{};
+private:
+	explicit TestObject() = default;
+	virtual ~TestObject() = default;	
+	// GameObject을(를) 통해 상속됨
+	virtual void Free() override;
+
+public:
+	static TestObject* Create();
+public:
+	virtual void    RenderForwardAlphaBlendImplementation(const ImplementationInfo&_ImplInfo)override;
+	virtual void    RenderReady()                          override;
+public:
+	virtual HRESULT Ready() override;
+	virtual HRESULT Awake() override;
+	virtual HRESULT Start() override;
+	virtual UINT    Update(const float _fDeltaTime) override;
+	virtual UINT    LateUpdate(const float _fDeltaTime) override;
+	virtual void	OnEnable() override;
+	virtual void    OnDisable() override;
+};
+#endif //
